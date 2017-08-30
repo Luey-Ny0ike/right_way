67a032fdf4099276bf5e92a29946971c83875459
lewis nyoike <nyoikelewis@gmail.com>
2017-08-30 10:55:38 +0300

    Created the association between posts and comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  has_many :comments
   has_attached_file :image, styles: { large: '600x 600>', medium: '300x300>', thumb: '150x150#' }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
