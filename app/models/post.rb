5e8f66f05fab4fade8de92398425e917ae0cddd8
lewis nyoike <nyoikelewis@gmail.com>
2017-08-31 16:01:35 +0300

    Defined a search method to find a post by title

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,12 @@ class Post < ApplicationRecord
   belongs_to :user
   has_attached_file :image, styles: { large: '600x 600>', medium: '300x300>', thumb: '150x150#' }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  def self.search(search)
+    if search
+      where('title LIKE ?', "%#{search}%")
+    else
+      unscoped
+    end
+  end
 end
